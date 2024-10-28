9226e0ac7ad1f50248516e85770f244a9187298e
614925 <senter.614925@swl.k12.oh.us>
2024-10-28 08:56:21 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        double celsius = 0;
+        double fahrenheit;
+        boolean valid = false;
+        String user;
+        String again = "N";
+
+        boolean run = true;
+
+        while (run){
+
+                Scanner scan = new Scanner(System.in);
+                do {
+                    System.out.printf("%-13s", "Please enter your temperature in celsius to be converted to fahrenheit");
+                    System.out.println();
+                    if (scan.hasNextDouble()) {
+                        celsius = scan.nextDouble();
+
+                        if (celsius >= -273.15) {
+                            valid = true;
+                            fahrenheit = (celsius * 1.8) + 32;
+
+                            System.out.printf("%-13s %-6.2f", "Degrees C:", celsius);
+                            System.out.printf("\n%-13s %-6.2f", "Degrees F:", fahrenheit);
+                            System.out.println();
+                        }
+                        else {
+                            System.out.println("You have entered an incorrect temperature");
+                        }
+                    }
+                    else {
+                        user = scan.nextLine();
+                        if (user.equals(again)) {
+                            run = false;
+                        }
+                        else {
+                            System.out.println("You Must enter a valid temperature");
+                        }
+                    }
+                } while (!valid);
+
+
+
+        }
     }
 }
